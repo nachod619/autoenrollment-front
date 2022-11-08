614d5b98b674d9ee6ef548e1f0f80e163ae352c9
Catalina <catalina.poncep@utem.cl>
2022-11-07 21:36:59 -0300

    add: empresa sign up form

--- vue_base-master/src/views/ClientForm.vue
+++ vue_base-master/src/views/ClientForm.vue
@@ -1,7 +1,70 @@
-<template>
-  <div>
-    <h1>Formulario empresa</h1>
- </div>
+<template >
+  <v-container>
+    <v-card class="mx-auto my-12" max-width="1200" style="box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px">
+    <v-container class="justify-center" pa-8>
+      <v-img
+        contain
+        max-height="100"
+        max-width="215"
+        class="mx-auto py-4"
+        src="https://alloxentric.com/wp-content/uploads/2020/11/alloxentric_logo-3x.png"
+      >
+      </v-img>
+      <v-card-title class="justify-center py-7">
+        <div style="flex: auto; text-align-last: center">Formulario empresa</div>
+      </v-card-title>
+      <v-container class="py-4">
+        <v-row>
+          <v-col class="" >
+            <v-container style="height: fit-content">
+              <p class="text-center font-weight-medium" style="font-size: 20px;">
+                Asociarme a una empresa
+              </p>
+              <v-container class="py-6 px-4">
+                <v-text-field label="Numero identificador de la empresa"></v-text-field>
+                <v-text-field label="Nombre de la empresa"></v-text-field>
+              </v-container>
+              <v-container class="d-flex justify-center">
+                <v-btn
+                  depressed
+                  color="#06b7b2"
+                  class="white--text"	
+                >
+                  Asociarme
+                </v-btn>
+              </v-container>
+            </v-container>
+            
+          </v-col>
+          <v-divider
+            vertical
+          ></v-divider>
+          <v-col>
+            <p class="text-center font-weight-medium" style="font-size: 20px;">
+              Crear una nueva empresa
+            </p>
+            <v-container class="py-6 px-4">
+              <v-text-field label="Nombre de la empresa"></v-text-field>
+              <v-text-field label="Numero de identificación"></v-text-field>
+              <v-text-field label="Descripción de la empresa"></v-text-field>
+              <v-text-field label="Descripción"></v-text-field>
+            </v-container>
+            <v-container class="d-flex justify-center">
+              <v-btn
+                depressed
+                color="#06b7b2"
+                class="white--text"
+              >
+                Crear
+              </v-btn>
+            </v-container>
+            
+          </v-col>
+         </v-row>
+        </v-container>
+      </v-container>
+    </v-card>     
+</v-container>
 </template>
 <script lang="ts">
  import { Component, Vue } from 'vue-property-decorator';
